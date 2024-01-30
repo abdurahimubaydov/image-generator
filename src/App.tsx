b0ab25346a228644a35f65a78fc82c42a580513e
user <user@192.168.1.2>
2024-01-30 17:21:45 +0500

    added to git

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export default function App(): JSX.Element {
+
+  const number: Number | string = '20';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {number}
     </div>
   );
-}
-
-export default App;
+};
